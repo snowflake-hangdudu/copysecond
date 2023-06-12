e2cedc3f50a7d67319952a0e87964d8744b025ab
weihaoran <15551359775@163.com>
2023-06-13 01:49:53 +0800

    1

--- src/views/debug/vue/index.vue
+++ src/views/debug/vue/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <h1>{{ name }}</h1>
-    <button @click="get">点我</button>
-    <input type="text" placeholder="按下按键提示输入" @keyup.enter="showInfo">
+    搜索姓名:<input v-model="keyword" type="text">
+    <ul>
+      <li v-for="(l,i) in filtList" :key="i">{{ l.name }} - {{ l.age }} - {{ l.gender }}</li>
+    </ul>
+    <button>年龄升序</button>
+    <button>年龄降序</button>
+    <button>原顺序</button>
+
   </div>
 </template>
 
@@ -11,22 +17,41 @@ export default {
   name: 'Vue',
   data() {
     return {
-      name: 'hangdudu'
+      name: 'hangdudu',
+      list: [
+        { name: '张三', age: 20, gender: 'male' },
+        { name: '李四', age: 25, gender: 'female' },
+        { name: '王五', age: 30, gender: 'male' },
+        { name: '赵六', age: 35, gender: 'female' }
+      ],
+      keyword: ''
+      // filtList: []
+
     }
   },
   computed: {
-
+    filtList() {
+      const keyword = this.keyword
+      if (keyword === '') {
+        return this.list
+      }
+      return this.list.filter(item => {
+        return item.name.indexOf(keyword) > -1
+      })
+    }
   },
   watch: {
-
+    // keyword: {
+    //   immediate: true,
+    //   handler(val) {
+    //     this.filtList = this.list.filter(item => {
+    //       return item.name.indexOf(val) > -1
+    //     })
+    //   }
+    // }
   },
   methods: {
-    get() {
-      alert('hello')
-    },
-    showInfo(e) {
-      console.log(e.target.value)
-    }
+
   }
 }
 
