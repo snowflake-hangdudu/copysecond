4e992c7b368c5beba4f5c0475377e550ed5846af
weihaoran <15551359775@163.com>
2023-06-24 21:46:19 +0800

    提交

--- src/views/debug/html/index.vue
+++ src/views/debug/html/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-
+    <h1>hangdudu</h1>
   </div>
 </template>
 
@@ -8,6 +8,18 @@
 export default {
   name: 'Html'
 }
+
+const obj = {
+  name: 'hangdudu',
+  age: 22
+}
+
+let f =function f() {
+  console.log(521)
+}
+
+console.log(f.length)
+console.log()
 </script>
 
 <style >
